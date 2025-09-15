b396315c037ad0d61e0b40904241ffb0fbad2efd
LuisCMoreira <luiscarlos.f.moreira@gmail.com>
2025-09-15 23:15:02 +0100

    sdcard

--- Rexroth/ctrlX/0 - Tutorials/ctrlXOPCUAserver/opcuaClientGetwithAuth.py
+++ Rexroth/ctrlX/0 - Tutorials/ctrlXOPCUAserver/opcuaClientGetwithAuth.py
@@ -5,14 +5,14 @@ sys.path.append('./packages')
 from opcua import Client, ua
 
 # Define the OPC UA server endpoint URL
-url = "opc.tcp://192.168.1.1:4840/"
+url = "opc.tcp://192.168.1.2:4840/"
 
 # Create a client object
 client = Client(url)
 
 # Set the username and password for authentication
 username = "boschrexroth"
-password = "Equinotec2024"
+password = "boschrexroth"
 client.set_user(username)
 client.set_password(password)
 
@@ -33,11 +33,11 @@ try:
 
     # Define the namespace index and node paths
     namespace_index= 2
-    node_path_toRead  = "plc/app/Application/sym/PLC_PRG/iCounter"
-    node_path_toWrite = "plc/app/Application/sym/PLC_PRG/boolVar2"
+    node_path_toRead  = "system/health/memory-available/value"
+    #node_path_toWrite = "plc/app/Application/sym/PLC_PRG/boolVar2"
 
     # Create a node ID for the variable
-    node_id_toWrite = ua.NodeId(node_path_toWrite, namespace_index)
+    #node_id_toWrite = ua.NodeId(node_path_toWrite, namespace_index)
 
     # Create a node ID for the variable
     node_id_toRead  = ua.NodeId(node_path_toRead , namespace_index )
@@ -53,17 +53,17 @@ try:
 
 
         # check node variable type if necessaty
-        node_to_write = client.get_node(node_id_toWrite)
-        data_type = node_to_write.get_data_type_as_variant_type()
-        print(f"Data type of node '{node_path_toWrite}' is {data_type}")
+        #node_to_write = client.get_node(node_id_toWrite)
+        #data_type = node_to_write.get_data_type_as_variant_type()
+        #print(f"Data type of node '{node_path_toWrite}' is {data_type}")
 
         # Read the value of the node
-        try:
-            variable_node = client.get_node( node_to_write )
-            value = variable_node.get_value()
-            print(f"Value of node '{ node_to_write }': {value}")
-        except ua.UaStatusCodeError as e:
-            print(f"Error accessing node '{ node_to_write }': {e}")
+        #try:
+        #    variable_node = client.get_node( node_to_write )
+        #    value = variable_node.get_value()
+        #    print(f"Value of node '{ node_to_write }': {value}")
+        #except ua.UaStatusCodeError as e:
+        #    print(f"Error accessing node '{ node_to_write }': {e}")
 
         # Set new value to differ from the last
         if value == True:
@@ -72,14 +72,14 @@ try:
             new_value = True
         
         # Write the value of the node
-        try:
-            variable_node.set_value(ua.Variant(new_value, ua.VariantType.Boolean))
-            print(f"Successfully wrote {new_value} to node '{node_path_toWrite}'.")
-        except ua.UaStatusCodeError as e:
-            print(f"Error writing to node '{node_path_toWrite}': {e}")
+        #:
+        #    variable_node.set_value(ua.Variant(new_value, ua.VariantType.Boolean))
+        #    print(f"Successfully wrote {new_value} to node '{node_path_toWrite}'.")
+        #except ua.UaStatusCodeError as e:
+        #    print(f"Error writing to node '{node_path_toWrite}': {e}")
 
         # Wait before new cycle
-        time.sleep(1)
+        #time.sleep(1)
 
 finally:
     # Disconnect from the OPC UA server if connected
